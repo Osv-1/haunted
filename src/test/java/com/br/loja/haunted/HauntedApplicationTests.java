31354c57ef3287bf8d15bba8807adc218ebdab7c
Takeo <oswaldotakeo1@gmail.com>
2022-05-03 12:31:00 -0300

    Initial commit

---
+++ src/test/java/com/br/loja/haunted/HauntedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.loja.haunted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauntedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
